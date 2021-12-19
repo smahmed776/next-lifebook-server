9f326bcf3f20ac4024653e2914ee4238b60a8bd7
Syed Mahbub <xsmahmed776@gmail.com>
2021-12-19 22:06:10 +0600

    admin update prblm

--- routes/admin/adminupdate.js
+++ routes/admin/adminupdate.js
@@ -2,10 +2,13 @@ const Users = require("../../schemas/UserSchema");
 
 exports.updateUser = async (req, res) => {
   const userId = req.userId.id;
+  console.log(userId)
   const isAdmin = await Users.findOne({ _id: userId }, ["type"]);
+  console.log(isAdmin)
   if (isAdmin.type === "admin") {
     const { id } = req.params;
     const { verified } = req.body;
+    console.log("working", id)
     if (id) {
       await Users.findOneAndUpdate(
         { _id: id },
