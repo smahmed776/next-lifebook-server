6e77dc5c8574872cd5514a3e7164b43ad8652539
Syed Mahbub <xsmahmed776@gmail.com>
2021-12-19 22:09:27 +0600

    remove console logs

--- routes/admin/adminupdate.js
+++ routes/admin/adminupdate.js
@@ -2,13 +2,10 @@ const Users = require("../../schemas/UserSchema");
 
 exports.updateUser = async (req, res) => {
   const userId = req.userId.id;
-  console.log(userId)
   const isAdmin = await Users.findOne({ _id: userId }, ["clearance"]);
-  console.log(isAdmin)
   if (isAdmin.clearance === "admin") {
     const { id } = req.params;
     const { verified } = req.body;
-    console.log("working", id)
     if (id) {
       await Users.findOneAndUpdate(
         { _id: id },
